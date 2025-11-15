e8aaa014798f439c6b9ad31cbcfa1a256c283ec2
Akrist Gautam <144605978+Akrist3@users.noreply.github.com>
2025-11-15 09:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (20.68%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans(2*n);
-        int indx = 0;
+        vector<int> ans(2*n,0);
+        int i = 0;
+        int j = n;
         for (int i = 0; i < n; i++) {
-            ans[indx++] = nums[i];
-            ans[indx++] = nums[i + n];
+            ans[i*2] = nums[i];
+            ans[i*2 +1] = nums[j];
+            j++;
         }
         return ans;        
     }

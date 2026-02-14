a201f14a850cf34cf1763382e6ad5bc8e4af1dcc
Akrist Gautam <144605978+Akrist3@users.noreply.github.com>
2026-02-14 22:22:57 +0530

    Time: 9 ms (49.72%) | Memory: 33.4 MB (56.69%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,17 +1,25 @@
+// this is a bfs Approach 
 class Solution {
 private:
-    void dfs(int row,int col,vector<vector<int>> &vis ,vector<vector<int>> &grid){
+    void bfs(int row,int col,vector<vector<int>> &vis ,vector<vector<int>> &grid){
         vis[row][col]= 1;
         int n = grid.size();
         int m = grid[0].size();
         int delrow[] = {-1,0,+1,0};
         int delcol[] = {0,+1,0,-1};
-
-        for(int i = 0;i<4;i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if(nrow >= 0 && nrow <n && ncol >= 0 && ncol <m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                dfs(nrow,ncol,vis,grid);
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        while(!q.empty()){
+            int row = q.front().first;            
+            int col = q.front().second; 
+            q.pop();           
+            for(int i = 0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow >= 0 && nrow <n && ncol >= 0 && ncol <m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol] = 1;
+                }
             }
         }
     }
@@ -21,20 +29,10 @@ public:
         int m = grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i = 0;i<n;i++){
-            if(!vis[i][0] && grid[i][0] == 1){
-                dfs(i,0,vis,grid);
-            }
-            if(!vis[i][m-1] && grid[i][m-1] == 1){
-                dfs(i,m-1,vis,grid);
-            }
-        }
-        for(int j = 0;j<m;j++){
-            if(!vis[0][j] && grid[0][j] == 1){
-                dfs(0,j,vis,grid);
-            }
-        
-            if(!vis[n-1][j] && grid[n-1][j] == 1){
-                dfs(n-1,j,vis,grid);
+            for(int j = 0;j<m;j++){
+                if((i == 0|| i == n-1 || j == 0 ||j == m-1) && grid[i][j] == 1 && vis[i][j] == 0){
+                    bfs(i,j,vis,grid);                    
+                }
             }
         }
         int cnt = 0;

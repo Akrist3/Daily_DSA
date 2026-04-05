14a1639f7b5c559bbf02b7e3c9749c6c9b8d055d
Akrist Gautam <144605978+Akrist3@users.noreply.github.com>
2026-04-05 07:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (52.53%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,13 +1,4 @@
 class Solution {
-    int solve(int n,int m,vector<vector<int>>& obs,vector<vector<int>>& dp){
-        if(n < 0 || m < 0 ) return 0;
-        if(obs[n][m] == 1 ) return 0;
-        if(n == 0 && m == 0) return 1;
-        if(dp[n][m] != -1) return dp[n][m];
-        int up = solve(n-1,m,obs,dp);
-        int left = solve(n,m-1,obs,dp);
-        return dp[n][m] = up+left;
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obs) {
         int n = obs.size();

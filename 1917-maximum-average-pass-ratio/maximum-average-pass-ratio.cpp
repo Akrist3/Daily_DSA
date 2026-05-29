2ba9d28805903143abe65d12276e76e2fd33aa5d
Akrist Gautam <144605978+Akrist3@users.noreply.github.com>
2026-05-29 22:29:12 +0530

    Time: 382 ms (19.03%) | Memory: 99.1 MB (21.75%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    double gain(int p, int t) {
+        return (double)(p + 1) / (t + 1) - (double)p / t;
+    }
+
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+
+        priority_queue<
+            tuple<double, int, int>
+        > pq;
+
+        for (auto &c : classes) {
+            pq.push({gain(c[0], c[1]), c[0], c[1]});
+        }
+
+        while (extraStudents--) {
+            auto [g, p, t] = pq.top();
+            pq.pop();
+
+            p++;
+            t++;
+
+            pq.push({gain(p, t), p, t});
+        }
+
+        double sum = 0;
+
+        while (!pq.empty()) {
+            auto [g, p, t] = pq.top();
+            pq.pop();
+
+            sum += (double)p / t;
+        }
+
+        return sum / classes.size();
+    }
+};

86463d6ccdb85d2d433041493f5fd7e1ba511de1
Akrist Gautam <144605978+Akrist3@users.noreply.github.com>
2026-06-24 06:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (34.12%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,19 +6,13 @@ public:
 
         while (l < r) {
             int mid = l + (r - l) / 2;
-
-            // Make sure mid is even (because pairs start at even indices)
             if (mid % 2 == 1) mid--;
-
-            // If the pair is valid, move right
             if (nums[mid] == nums[mid + 1]) {
                 l = mid + 2;
             } else {
                 r = mid;
             }
         }
-
-        // l will stop at the single element
         return nums[l];
     }
 };

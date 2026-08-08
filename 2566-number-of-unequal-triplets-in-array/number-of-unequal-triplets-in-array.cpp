0793761287b4607739e94c6fb5c73ec5cdbb3669
Akrist Gautam <144605978+Akrist3@users.noreply.github.com>
2026-08-08 06:50:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (27.59%) - LeetSync

--- 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
+        unordered_map<int, int> mp;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] != nums[j] &&
-                        nums[j] != nums[k] &&
-                        nums[i] != nums[k]) {
-                        count++;
-                    }
-                }
-            }
+        for (int x : nums) {
+            mp[x]++;
         }
 
-        return count;
+        int left = 0;
+        int ans = 0;
+        int n = nums.size();
+
+        for (auto& [value, freq] : mp) {
+            int right = n - left - freq;
+
+            ans += left * freq * right;
+
+            left += freq;
+        }
+
+        return ans;
     }
 };
